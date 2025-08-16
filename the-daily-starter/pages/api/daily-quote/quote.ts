fe8531dfef81466ed0011752c7bfdf1a358dd99b
bmstoss13 <123670110+bmstoss13@users.noreply.github.com>
2025-08-16 19:12:02 -0400

    firebase works as intended thus far

--- the-daily-starter/pages/api/daily-quote/quote.ts
+++ the-daily-starter/pages/api/daily-quote/quote.ts
@@ -5,33 +5,33 @@ import { fetchDailyQuote } from "@/lib/zenquotes";
 import { collection, doc, getDoc, setDoc } from "firebase/firestore";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // Check that the request method is GET
-  if (req.method !== 'GET') {
-    return res.status(405).json({ message: 'Method Not Allowed' });
-  }
-
-  const today = new Date().toISOString().split("T")[0]; // get today's date
-
-  try {
-    const ref = doc(collection(db, "dailyQuotes"), today);
-    const snapshot = await getDoc(ref);
-
-    // Check if the daily quote already exists for today in the db.
-    if (snapshot.exists()) {
-      return res.status(200).json(snapshot.data());
+    // Check that the request method is GET
+    if (req.method !== 'GET') {
+        return res.status(405).json({ message: 'Method Not Allowed' });
     }
 
-    // Call the ZenQuotes API to fetch a new quote.
-    const quote = await fetchDailyQuote();
-    
-    // Save the new quote to the database.
-    await setDoc(ref, { ...quote, date: today });
-    
-    // Return the new quote as a JSON response.
-    return res.status(200).json(quote);
-  } catch (err) {
-    console.error("Error getting daily quote at api endpoint, " + err);
-    // Return an error response.
-    return res.status(500).json({ error: "Failed to fetch daily quote" });
-  }
+    const today = new Date().toISOString().split("T")[0]; // get today's date
+
+    try {
+        const ref = doc(collection(db, "dailyQuotes"), today);
+        const snapshot = await getDoc(ref);
+
+        // Check if the daily quote already exists for today in the db.
+        if (snapshot.exists()) {
+            return res.status(200).json(snapshot.data());
+        }
+
+        // Call the ZenQuotes API to fetch a new quote.
+        const quote = await fetchDailyQuote();
+        
+        // Save the new quote to the database.
+        await setDoc(ref, { ...quote, date: today });
+        
+        // Return the new quote as a JSON response.
+        return res.status(200).json(quote);
+    } catch (err) {
+        console.error("Error getting daily quote at api endpoint, " + err);
+        // Return an error response.
+        return res.status(500).json({ error: "Failed to fetch daily quote" });
+    }
 }
