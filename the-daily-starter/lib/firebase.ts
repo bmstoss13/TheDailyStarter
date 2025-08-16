2bcc3622987b1f86cd469560635cf3423ed4ee75
bmstoss13 <123670110+bmstoss13@users.noreply.github.com>
2025-08-16 17:57:55 -0400

    auth

--- the-daily-starter/lib/firebase.ts
+++ the-daily-starter/lib/firebase.ts
@@ -21,7 +21,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
-const db = getFirestore(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 
 const analytics = getAnalytics(app);
