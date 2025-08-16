072a83f78f4fa202bce1c8d615dc7c262294942f
bmstoss13 <123670110+bmstoss13@users.noreply.github.com>
2025-08-16 18:26:50 -0400

    auth form

---
+++ the-daily-starter/components/AuthForm.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useState } from "react";
+import { auth } from "@/lib/firebase";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
+//Auth form for when users want to join or sign in.
+export default function AuthForm() {
+    const [email, setEmail ] = useState("");
+    const [password, setPassword] = useState("");
+    const [isLogin, setIsLogin] = useState(true);
+    
+    async function handleSubmit(e: React.FormEvent){
+        e.preventDefault();
+        try{
+            if(isLogin){
+                await signInWithEmailAndPassword(auth, email, password);
+            }
+            else{
+                await createUserWithEmailAndPassword(auth, email, password);
+            }
+        } catch (err) {
+            console.error("Error occurred while handling submit: ", err);
+        }
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>{isLogin ? "Sign In" : "Create Account"}</h2>
+            <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                value={email}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">
+                {isLogin ? "Sign In" : "Sign Up"}
+            </button>
+            <p
+                onClick={() => setIsLogin(!isLogin)}
+            >
+                {isLogin ? "Create an account" : "Have an account? Sign in!"}
+            </p>
+        </form>
+    )
+}
