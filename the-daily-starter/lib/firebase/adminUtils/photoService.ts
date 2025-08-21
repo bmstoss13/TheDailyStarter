252a48b3bc37e1756a9bdeb7d7636c1787584457
bmstoss13 <123670110+bmstoss13@users.noreply.github.com>
2025-08-21 12:47:48 -0400

    formidable

--- the-daily-starter/lib/firebase/adminUtils/photoService.ts
+++ the-daily-starter/lib/firebase/adminUtils/photoService.ts
@@ -3,10 +3,14 @@ import { photoCollection } from "../collectionNames";
 import { PhotoData } from "../interfaces";
 import { photoBucket } from "../storage/bucketNames";
 
-/*function to upload (POST) photo and store in firebase storage.
-Should take in the uid and the file to be uploaded. 
-return the photo data (metadata)
-*/
+//helper for getting the project id
+function getProjectId(){
+    const projectId = admin.app().options.projectId;
+    if(typeof projectId !== 'string') {
+        return '';
+    }
+    return projectId;
+}
 
 // uploads a photo file to firebase storage and stores metadata in firestore.
 export async function uploadAndStorePhoto(uid: string, file: File): Promise<PhotoData | null> {
@@ -19,10 +23,75 @@ export async function uploadAndStorePhoto(uid: string, file: File): Promise<Phot
             },
         });
 
-        console.log(`Successfully uploaded photo`);
-        return null;
+        console.log(`Successfully uploaded photo: ${uploadResult}`);
+
+        const [url] = await photoRef.getSignedUrl({
+            action: 'read',
+            expires: '11/12/3002',
+        });
+
+        console.log(`Download URL generated: ${url}`);
+
+        const photoData: PhotoData = {
+            url: url,
+            fileName: file.name,
+            uploadedBy: uid,
+            createdAt: admin.firestore.FieldValue.serverTimestamp(),
+        };
+
+        const docRef = await db.collection(`artifacts`).doc(getProjectId()).collection("users").doc(uid).collection(photoCollection).add(photoData);
+        console.log(`Successfully stored photo metadata in Firestore with id: ${docRef.id}`);
+
+        return photoData;
+
     } catch (err: any) {
         console.error("An error occurred while uploading photo: ", err);
         throw new Error(err.message || "Error uploading photo.");
+    }
+}
+
+//Fetch all photo urls for a given user
+export async function getPhotosForUser(uid: string): Promise<PhotoData[]> {
+    try{
+        const photoRef = db.collection(`artifacts`).doc(getProjectId()).collection("users").doc(uid).collection(photoCollection)
+        const snapshot = await photoRef.orderBy('createdAt', 'desc').get();
+
+        if (snapshot.empty){
+            console.log('No photos found for user: ', uid);
+            return [];
+        }
+
+        const orderedPhotos: PhotoData[] = [];
+        snapshot.forEach(doc => {
+            orderedPhotos.push({
+                ...doc.data() as PhotoData,
+                createdAt: doc.data().createdAt,
+            });
+        });
+
+        return orderedPhotos;
+    } catch (err: any){
+        console.error(`An error occurred while fetching photos for user: ${uid}`);
+        throw new Error(err.message || "Error while getting photos for user.");
+    }
+}
+
+export async function getSinglePhotoForUser(uid: string): Promise<PhotoData|null> {
+    try{
+        const photoRef = db.collection("artifacts").doc(getProjectId()).collection("users").doc(uid).collection(photoCollection);
+        const snapshot = await photoRef.orderBy('createdAt', 'desc').limit(1).get();
+
+        if(snapshot.empty) {
+            console.log(`No photos found for user: ${uid}`);
+            return null;
+        }
+
+        const doc = snapshot.docs[0];
+
+
+        return { ...doc.data() as PhotoData, createdAt: doc.data().createdAt, };
+    } catch (err: any){
+        console.error(`An error occurred while fetching this photo for user: ${uid}`)
+        throw new Error(err.message || 'Error while fetching photo for user.')
     }
 }
--- the-daily-starter/lib/firebase/interfaces.ts
+++ the-daily-starter/lib/firebase/interfaces.ts
@@ -58,7 +58,7 @@ export interface RayData {
 //photo data for reference to firebase storage bucket
 export interface PhotoData {
     url: string;
-    filename: string;
+    fileName: string;
     uploadedBy: string;
     createdAt: admin.firestore.FieldValue; //need to refactor everything else to get accurate time snippets.
 }
